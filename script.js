b29d5e27181aa12e161c3cbd8af816a8b78955d3
Oleg Khaskin <okhaskin@gmail.com>
2023-10-17 09:09:01 +0300

    Update script.js
    disable double clicking on send button

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const textBox = document.createElement("TextBoxInsertQuestion");
 const AnswerShow = document.createElement("TextBoxShowAnswer");
 const SendButton = document.querySelector('#SendButton');
 
+MsgWaitForAnswer = 'Please wait few seconds, embedding your question...'
+
 ServerAddress = 'https://014e-194-163-191-105.ngrok-free.app'
 //ServerAddress = 'https://cbcd-77-137-75-12.ngrok.io'
 
@@ -14,8 +16,13 @@ ServerAddress = 'https://014e-194-163-191-105.ngrok-free.app'
 document.body.appendChild(textBox);
 
 // Event listener for file selection
+fileInput.addEventListener('click', handleChooseFileForUpload);
 loadButton.addEventListener('click', handleFileUpload);
 SendButton.addEventListener('click', handleSendButton);
+
+function handleChooseFileForUpload(){
+    loadButton.textContent = 'Load';
+}
 
 function handleSendButton() {
 // Create a FormData object
@@ -24,28 +31,29 @@ function handleSendButton() {
   var ShowAnswer = document.getElementById("TextBoxShowAnswer")
 
   formData.append('text', text);
-  ShowAnswer.value = 'Please wait few seconds, embedding your question...'
+  if (ShowAnswer.value != MsgWaitForAnswer){
+      ShowAnswer.value = MsgWaitForAnswer
 
-  // Send the file data to the Streamlit backend
-  //fetch('http://localhost:8888/question', {
-  fetch(ServerAddress + '/question', {
-    method: 'POST',
-    body: formData
-    //body: formData
-  })
-  .then(response => response.text())
-  //.then(response => JSON.stringify(response.json()))
-  .then(response => {
-    // Process and display the response data
-    console.log(response);
-    ShowAnswer.value = response;
-    // Update the UI with the processed results
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    ShowAnswer.value = 'error: ' + error;
-    // Handle error scenarios
-  });
+      fetch(ServerAddress + '/question', {
+            method: 'POST',
+            body: formData
+      })
+      .then(response => response.text())
+      .then(response => {
+        // Process and display the response data
+        console.log(response);
+        ShowAnswer.value = response;
+        // Update the UI with the processed results
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        ShowAnswer.value = 'error: ' + error;
+        // Handle error scenarios
+      });
+  }
+  else{
+    ShowAnswer.value = MsgWaitForAnswer
+  }
 }
 
 // Function to handle file upload
@@ -56,6 +64,8 @@ function handleFileUpload() {
   const formData = new FormData();
   formData.append('file', file);
 
+  loadButton.textContent = 'Uploading...';
+  
   // Send the file data to the Streamlit backend
   //fetch('http://localhost:8888/upload', {
   fetch(ServerAddress + '/upload', {
@@ -72,4 +82,6 @@ function handleFileUpload() {
     console.error('Error:', error);
     // Handle error scenarios
   });
+  
+  loadButton.textContent = 'Finished uploading.';
 }
